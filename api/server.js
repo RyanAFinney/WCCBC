e49c7f137be930a44d3e2169e54e080d65929caa
Ryan Finney <ryanafinney@gmail.com>
2019-04-09 15:22:55 -0400

    add mongodb

--- api/server.js
+++ api/server.js
@@ -2,7 +2,8 @@ const express = require('express'),
       path = require('path'),
       bodyParser = require('body-parser'),
       cors = require('cors'), 
-      mongoose = require('mongoose');
+      mongoose = require('mongoose'),
+      config = require('./DB');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true}).then(
