a0b48ef94b30c6254fbbdb12fe12fe534fc8b12f
Ryan Finney <ryanafinney@gmail.com>
2019-04-09 15:15:46 -0400

    import more libraries and write add functions

---
+++ src/app/Business.ts
@@ -0,0 +1,6 @@
+//Business.ts
+export default class Business {
+    person_name: String;
+    business_name: String;
+    business_gst_number: Number;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { GstAddComponent } from './gst-add/gst-add.component';
 import { GstGetComponent } from './gst-get/gst-get.component';
 import { GstEditComponent } from './gst-edit/gst-edit.component';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BusinessService } from './business.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,12 @@ import { GstEditComponent } from './gst-edit/gst-edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BusinessService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
